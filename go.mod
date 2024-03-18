b2b0fe799e9a1d500248b2a4165e1bd9ce9b7197
Arnav Rupde <34592221+ArnavRupde@users.noreply.github.com>
2024-03-18 20:12:55 +0530

    Add JWT authen

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module example.com/server
 
 go 1.22.1
 
-require github.com/gofiber/fiber/v2 v2.52.2
+require (
+	github.com/gofiber/contrib/jwt v1.0.8
+	github.com/gofiber/fiber/v2 v2.52.2
+	github.com/golang-jwt/jwt/v5 v5.2.1
+)
 
 require (
+	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+
+	jwtware "github.com/gofiber/contrib/jwt"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type User struct {
@@ -17,6 +21,15 @@ type Login struct {
 	Password string `json:"password"`
 }
 
+var SECRET_KEY = "abc123"
+
+// Middleware for JWT
+func NewAuthMiddleware(secret string) fiber.Handler {
+	return jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte(secret)},
+	})
+}
+
 func main() {
 
 	// Create a slice with users
@@ -26,6 +39,8 @@ func main() {
 	}
 
 	app := fiber.New() // Notice similarity with express.js => const app = express()
+
+	jwtMiddleware := NewAuthMiddleware(SECRET_KEY)
 
 	// Return simple string
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -38,7 +53,7 @@ func main() {
 	})
 
 	// Post requesting accepting Json data
-	app.Post("/users", func(c *fiber.Ctx) error {
+	app.Post("/users", jwtMiddleware, func(c *fiber.Ctx) error {
 		user := new(User)
 		err := c.BodyParser(user)
 		if err != nil {
@@ -46,6 +61,35 @@ func main() {
 		}
 		users = append(users, *user)
 		return c.JSON(users)
+	})
+
+	// Login route
+	app.Post("/login", func(c *fiber.Ctx) error {
+		loginCredentials := new(Login)
+		err := c.BodyParser(loginCredentials)
+		if err != nil && loginCredentials.Username != "" && loginCredentials.Password != "" {
+			return c.Status(400).SendString("Invalid input JSON")
+		}
+
+		username := "admin"
+		password := "admin"
+		if loginCredentials.Username == username && loginCredentials.Password == password {
+			// Set up content for JWT
+			claims := jwt.MapClaims{
+				"username": username,
+				"exp":      time.Now().Add(time.Hour * 24).Unix(),
+			}
+			// Create token
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+			//Encode token
+			encodedToken, err := token.SignedString([]byte(SECRET_KEY))
+			if err != nil {
+				return c.SendStatus(500)
+			}
+			return c.JSON(fiber.Map{"token": encodedToken})
+		} else {
+			return c.SendStatus(401)
+		}
 	})
 
 	log.Fatal(app.Listen(":3001"))
