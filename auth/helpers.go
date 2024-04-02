b71b765d2a5cdbaeee39d55a3251fd87a230cf8c
Arnav Rupde <34592221+ArnavRupde@users.noreply.github.com>
2024-04-03 01:01:20 +0530

    Create auth helpers

---
+++ auth/helpers.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("Failed to hash password")
+	}
+	return string(hashedPassword), nil
+}
+
+func ComaparePassword(hashedPassword string, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		return fmt.Errorf("Incorrect password")
+	}
+	return nil
+}
